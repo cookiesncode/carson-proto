d250a2b26070e2bca37d6cfcdf6bae3f11a1f26b
Miguel Gomez <miguel.gomez16.ctr@mail.mil>
2017-11-14 14:36:32 -0700

    New method working. -Using template HTML -Using inline images in content feed

--- src/assets/js/lib/reporting-status.js
+++ src/assets/js/lib/reporting-status.js
@@ -3,50 +3,75 @@ var numberOfItems = 10; // These are the number of posts you want to retrieve
 var category = 5
 var feedUrl = 'http://www.fortcarsonmountaineer.com/wp-json/wp/v2/posts?categories=' + category + '&per_page=' + numberOfItems; 
 var imgUrl;
-var $targetArea = $('.community-events');
 var titles = [];
 var thumbnails = [];
 var links = [];
 var isTitlesLoaded = false;
 var isImageLoaded = false;
 
-function getImages(imgUrl) {
-  $.getJSON(imgUrl, function (data, textStatus, jqxhr) {
-    thumbnails.push(data['media_details']['sizes']['medium']['source_url']);
-  });
+function bindTemplate($template, feedItem) {
+  $template.find('.item_img').attr('src',feedItem.imgSrc);
+  $template.find('.item_url').attr('href', feedItem.url).text(feedItem.title);
+  return $template;
 }
-
-function createDomElem() {
-  var items = []
-  $.each(titles, function(key, val) {
-    items.push('<div class="cell"><div class="card"><div class="card-section"><img src="' + thumbnails[key] + '"></div><div class="card-section"><p><a href="' + links[key] + '">' + val + '</a></p></div></div></div>');
-  });
-  $targetArea.html(items.join(''));
-}
-
+var $newCellTemplate = $('.feeds .template');
 $.getJSON(feedUrl, function (data, textStatus, jqxhr) {
   if (textStatus === 'success') {
     $.each(data, function(key, val) {
-      imgUrl = val["_links"]['wp:featuredmedia']['0']['href'];
-      getImages(imgUrl);
-      titles.push(val.title.rendered)
-      links.push(val.link);
+      var $newCell = $newCellTemplate.clone().removeClass('template hide');
+      var img = $(val.content.rendered).find('img').eq(0).attr('src');
+      var feedItem = {
+        'title': val.title.rendered,
+        'imgSrc' : img,
+        'url': val.link
+      }
+      bindTemplate($newCell, feedItem)
+        .appendTo('.comm');
     });
+    $newCellTemplate.next('.loading-elem').remove();
   }
   else {
     $targetArea.html('<p>Uh oh, there is an issue getting the information right now.</p>');
   }  
 });
 
-$(document).ajaxComplete(function(event, request, settings){
-  console.log('ajaxCompleteHandlerCalled');
-  if (settings.url === feedUrl) {
-    isTitlesLoaded = true
-  }
-  else if (settings.url === imgUrl) {
-    isImageLoaded = true;
-  }
-  if (isTitlesLoaded && isImageLoaded && thumbnails.length === numberOfItems) {
-    createDomElem();
-  }
-});
+// $.getJSON(feedUrl, function (data, textStatus, jqxhr) {
+//   if (textStatus === 'success') {
+//     $.each(data, function(key, val) {
+//       imgUrl = val["_links"]['wp:featuredmedia']['0']['href'];
+//       getImages(imgUrl);
+//       titles.push(val.title.rendered)
+//       links.push(val.link);
+//     });
+//   }
+//   else {
+//     $targetArea.html('<p>Uh oh, there is an issue getting the information right now.</p>');
+//   }  
+// });
+
+// function getImages(imgUrl) {
+//   $.getJSON(imgUrl, function (data, textStatus, jqxhr) {
+//     thumbnails.push(data['media_details']['sizes']['medium']['source_url']);
+//   });
+// }
+
+// function createDomElem() {
+//   var items = []
+//   $.each(titles, function(key, val) {
+//     items.push('<div class="cell"><div class="card"><div class="card-section"><img src="' + thumbnails[key] + '"></div><div class="card-section"><p><a href="' + links[key] + '">' + val + '</a></p></div></div></div>');
+//   });
+//   $targetArea.html(items.join(''));
+// }
+
+// $(document).ajaxComplete(function(event, request, settings){
+//   console.log('ajaxCompleteHandlerCalled');
+//   if (settings.url === feedUrl) {
+//     isTitlesLoaded = true
+//   }
+//   else if (settings.url === imgUrl) {
+//     isImageLoaded = true;
+//   }
+//   if (isTitlesLoaded && isImageLoaded && thumbnails.length === numberOfItems) {
+//     createDomElem();
+//   }
+// });
