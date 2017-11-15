0b8fe8abee3012795368995d69db0284def057f3
Miguel Gomez <miguel.gomez16.ctr@mail.mil>
2017-11-15 16:33:05 -0700

    Initial scaffolding

--- src/assets/js/lib/reporting-status.js
+++ src/assets/js/lib/reporting-status.js
@@ -1,36 +1,47 @@
-import $ from 'jquery';
-var numberOfItems = 10; // These are the number of posts you want to retrieve
-var category = 5
-var feedUrl = 'http://www.fortcarsonmountaineer.com/wp-json/wp/v2/posts?categories=' + category + '&per_page=' + numberOfItems;
-var output = []; 
-var $pageTemplate = $('.feeds .template');
+import jQuery from 'jquery';
+(function($) {
 
-function outputHtml() {
-  $('.comm').html(output)
-}
-
-function bindTemplate($newCell, feedItem) {
-  $newCell.find('.item_img').attr('src',feedItem.imgSrc);
-  $newCell.find('.item_url').attr('href', feedItem.url).text(feedItem.title);
-  output.push($newCell.prop('outerHTML'));
-}
-
-$.getJSON(feedUrl, function (data, textStatus, jqxhr) {
-  if (textStatus === 'success') {
-    $.each(data, function(key, val) {
-      var $newCell = $pageTemplate.clone().removeClass('template hide');
-      var feedItem = {
-        'title': val.title.rendered,
-        'imgSrc' : $(val.content.rendered).find('img').eq(0).attr('src'),
-        'url': val.link
+  $.fn.mgFeeds = function() {
+    var $this = $(this);
+    var numberOfItems = 10; // These are the number of posts you want to retrieve
+    var category = 5
+    var feedUrl = 'http://www.fortcarsonmountaineer.com/wp-json/wp/v2/posts?categories=' + category + '&per_page=' + numberOfItems;
+    var output = []; 
+    var $pageTemplate = $('.feeds .template');
+  
+  
+    function outputHtml() {
+      $('.comm').html(output)
+    }
+  
+    function bindTemplate($newCell, feedItem) {
+      $newCell.find('.item_img').attr('src',feedItem.imgSrc);
+      $newCell.find('.item_url').attr('href', feedItem.url).text(feedItem.title);
+      output.push($newCell.prop('outerHTML'));
+    }
+  
+    $.getJSON(feedUrl, function (data, textStatus, jqxhr) {
+      if (textStatus === 'success') {
+        $.each(data, function(key, val) {
+          var $newCell = $pageTemplate.clone().removeClass('template hide');
+          var feedItem = {
+            'title': val.title.rendered,
+            'imgSrc' : $(val.content.rendered).find('img').eq(0).attr('src'),
+            'url': val.link
+          }
+          bindTemplate($newCell, feedItem);
+        });
+        outputHtml();
       }
-      bindTemplate($newCell, feedItem);
+      else {
+        $targetArea.html('<p>Uh oh, there is an issue getting the information right now.</p>');
+      }  
     });
-    outputHtml();
+    
+    return this.each(function() {
+      
+    });
+
   }
-  else {
-    $targetArea.html('<p>Uh oh, there is an issue getting the information right now.</p>');
-  }  
-});
 
-
+})(jQuery);
