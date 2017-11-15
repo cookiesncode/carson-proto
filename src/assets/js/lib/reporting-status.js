df7b93390d428d67e9122c523bf549c75db610b6
Miguel Gomez <miguel.gomez16.ctr@mail.mil>
2017-11-14 17:08:02 -0700

    Optimized DOM manipulation

--- src/assets/js/lib/reporting-status.js
+++ src/assets/js/lib/reporting-status.js
@@ -1,34 +1,33 @@
 import $ from 'jquery';
 var numberOfItems = 10; // These are the number of posts you want to retrieve
 var category = 5
-var feedUrl = 'http://www.fortcarsonmountaineer.com/wp-json/wp/v2/posts?categories=' + category + '&per_page=' + numberOfItems; 
-var imgUrl;
-var titles = [];
-var thumbnails = [];
-var links = [];
-var isTitlesLoaded = false;
-var isImageLoaded = false;
+var feedUrl = 'http://www.fortcarsonmountaineer.com/wp-json/wp/v2/posts?categories=' + category + '&per_page=' + numberOfItems;
+var output = []; 
+var $pageTemplate = $('.feeds .template');
 
-function bindTemplate($template, feedItem) {
-  $template.find('.item_img').attr('src',feedItem.imgSrc);
-  $template.find('.item_url').attr('href', feedItem.url).text(feedItem.title);
-  return $template;
+function outputHtml() {
+  $('.comm').html(output)
 }
-var $newCellTemplate = $('.feeds .template');
+
+function bindTemplate($newCell, feedItem) {
+  $newCell.find('.item_img').attr('src',feedItem.imgSrc);
+  $newCell.find('.item_url').attr('href', feedItem.url).text(feedItem.title);
+  output.push($newCell.prop('outerHTML'));
+}
+
 $.getJSON(feedUrl, function (data, textStatus, jqxhr) {
   if (textStatus === 'success') {
     $.each(data, function(key, val) {
-      var $newCell = $newCellTemplate.clone().removeClass('template hide');
+      var $newCell = $pageTemplate.clone().removeClass('template hide');
       var img = $(val.content.rendered).find('img').eq(0).attr('src');
       var feedItem = {
         'title': val.title.rendered,
-        'imgSrc' : img,
+        'imgSrc' : $(val.content.rendered).find('img').eq(0).attr('src'),
         'url': val.link
       }
-      bindTemplate($newCell, feedItem)
-        .appendTo('.comm');
+      bindTemplate($newCell, feedItem);
     });
-    $newCellTemplate.next('.loading-elem').remove();
+    outputHtml();
   }
   else {
     $targetArea.html('<p>Uh oh, there is an issue getting the information right now.</p>');
