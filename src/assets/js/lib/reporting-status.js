fcddb55158f35ee8b3aaa8f4a11d6c1706276ca3
Miguel Gomez <miguel.gomez16.ctr@mail.mil>
2017-11-15 11:20:34 -0700

    Cleaned up old code and unused variables.

--- src/assets/js/lib/reporting-status.js
+++ src/assets/js/lib/reporting-status.js
@@ -19,7 +19,6 @@ $.getJSON(feedUrl, function (data, textStatus, jqxhr) {
   if (textStatus === 'success') {
     $.each(data, function(key, val) {
       var $newCell = $pageTemplate.clone().removeClass('template hide');
-      var img = $(val.content.rendered).find('img').eq(0).attr('src');
       var feedItem = {
         'title': val.title.rendered,
         'imgSrc' : $(val.content.rendered).find('img').eq(0).attr('src'),
@@ -34,43 +33,4 @@ $.getJSON(feedUrl, function (data, textStatus, jqxhr) {
   }  
 });
 
-// $.getJSON(feedUrl, function (data, textStatus, jqxhr) {
-//   if (textStatus === 'success') {
-//     $.each(data, function(key, val) {
-//       imgUrl = val["_links"]['wp:featuredmedia']['0']['href'];
-//       getImages(imgUrl);
-//       titles.push(val.title.rendered)
-//       links.push(val.link);
-//     });
-//   }
-//   else {
-//     $targetArea.html('<p>Uh oh, there is an issue getting the information right now.</p>');
-//   }  
-// });
 
-// function getImages(imgUrl) {
-//   $.getJSON(imgUrl, function (data, textStatus, jqxhr) {
-//     thumbnails.push(data['media_details']['sizes']['medium']['source_url']);
-//   });
-// }
-
-// function createDomElem() {
-//   var items = []
-//   $.each(titles, function(key, val) {
-//     items.push('<div class="cell"><div class="card"><div class="card-section"><img src="' + thumbnails[key] + '"></div><div class="card-section"><p><a href="' + links[key] + '">' + val + '</a></p></div></div></div>');
-//   });
-//   $targetArea.html(items.join(''));
-// }
-
-// $(document).ajaxComplete(function(event, request, settings){
-//   console.log('ajaxCompleteHandlerCalled');
-//   if (settings.url === feedUrl) {
-//     isTitlesLoaded = true
-//   }
-//   else if (settings.url === imgUrl) {
-//     isImageLoaded = true;
-//   }
-//   if (isTitlesLoaded && isImageLoaded && thumbnails.length === numberOfItems) {
-//     createDomElem();
-//   }
-// });
