24119671747bafcd3397cc2722b94cfd602c3619
Miguel@Alien13R3 <gomail.gomez@gmail.com>
2017-11-17 10:42:33 -0700

    plugin update: - Moved default option out of plugin declaration to have global options access for user. - Added hideClass option

--- src/assets/js/lib/jquery.feedme.js
+++ src/assets/js/lib/jquery.feedme.js
@@ -7,18 +7,23 @@ Javascript details:
     $(selector).feedMe({
       category : 5,
       tags : 1110,
-      quantity : 10
+      quantity : 10,
+      hideClass: 'hide'
     });
 
 -List of options:
   1. category = The category ID you wish to filter the post by. The plugin only accepts a single category. The default is 'none'
   2. tags = The tag ID you wish to filter by. The plugin only accepts a single tag. The default is 'none' 
   3. quantity = The number of posts you wish to receive. The default is 10 and the max is 100.
+  4. hideClass = The class used to hide the [arent template element. Default is 'hide'
 
 - HTML details:
   - The HTML selection you pass to jQuery/$ must have a child element that will be used as your template. This element, usually a "div"  must have a data attribute called "data-feed-template". This element must also must have a class that sets it to display none. You can use the foundation 'hide' class to do this or you can use plain css to hide the template class.
   - The children of your template will use data attributes, data-feed-[datatype], in order to bind the return post data to it. 
-    Example: <h1 data-feed-title></h1>
+    Full Example: 
+    <div class="hide""" data-feed-template>
+      <h1 data-feed-title></h1>
+    </div>
 
   -List of data types available
     1. data-feed-link = Post link
@@ -32,16 +37,10 @@ Javascript details:
 import $ from 'jquery';
 import jQuery from 'jquery';
 (function($) {
-// to do
+
   $.fn.feedMe = function(options) {
     
-    $.fn.feedMe.defaults = {
-      category : 'none',
-      tags : 'none',
-      quantity : 10
-    };
     options = $.extend($.fn.feedMe.defaults, options);
-
     var $this = $(this);
     var categories = options.category !== 'none' ? '&categories=' + options.category : '';
     var tags = options.tags !== 'none' ? '&tags=' + options.tags : '';
@@ -68,7 +67,7 @@ import jQuery from 'jquery';
       $.getJSON(url, function (data, textStatus, jqxhr) {
         if (textStatus === 'success') {
           $.each(data, function(key, val) {
-            var $newTemplate = $template.clone().removeClass('template hide');
+            var $newTemplate = $template.clone().removeClass(options.hideClass).removeAttr('data-feed-template');
             var feedItem = {
               'title' : val.title.rendered,
               'imgSrc' : $(val.content.rendered).find('img').eq(0).attr('src'),
@@ -88,6 +87,13 @@ import jQuery from 'jquery';
       });
     });
 
+  }
+
+  $.fn.feedMe.defaults = {
+    category : 'none',
+    tags : 'none',
+    quantity : 10,
+    hideClass : 'hide'
   }
 
 })(jQuery);
