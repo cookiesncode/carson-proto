889e9be9187231bbe2d02de38976ced7dce451e2
Miguel Gomez <miguel.gomez16.ctr@mail.mil>
2017-11-20 16:16:39 -0700

    plugin update: - Plugin doesnt do double data bug on page - Still needs some work to prevent pulling json feed twice.

--- src/assets/js/lib/jquery.feedme.js
+++ src/assets/js/lib/jquery.feedme.js
@@ -1,5 +1,4 @@
 /*
-      
       --------------------INSTRUCTIONS---------------------
       Javascript details:
         - Make a jQuery selection and call .feedMe() on it. Example: $(selector).feedMe(); 
@@ -16,10 +15,10 @@
         1. category = The category ID you wish to filter the post by. The plugin only accepts a single category. The default is 'none'
         2. tags = The tag ID you wish to filter by. The plugin only accepts a single tag. The default is 'none' 
         3. quantity = The number of posts you wish to receive. The default is 10 and the max is 100.
-        4. hideClass = The class used to hide the [arent template element. Default is 'hide'
+        4. hideClass = The class used to hide the template element. Default is 'hide'
 
       - HTML details:
-        - The HTML selection you pass to jQuery/$ must have a child element that will be used as your template. This element, usually a "div"  must have a data attribute called "data-feed-template". This element must also must have a class that sets it to display none. You can use the foundation 'hide' class to do this or you can use plain css to hide the template class.
+        - The HTML selection you pass to jQuery/$ must have a child element that will be used as your template. This element, usually a "div"  must have a data attribute called "data-feed-template". This element must also must have a class that sets it to display none. You can use the foundation 'hide' class to do this or you can use your own custom class to hide it.
         - The children of your template will use data attributes, i.e. data-feed-[datatype], in order to bind the return post data to it. 
           Full Example: 
           <div class="hide" data-feed-template>
@@ -33,7 +32,6 @@
           3. data-feed-content = Post content body
           4. data-feed-excerpt = Post excerpt
           5. data-feed-img = The first available image inside the content body
-      
 */
 import $ from 'jquery';
 import jQuery from 'jquery';
@@ -47,59 +45,52 @@ import jQuery from 'jquery';
     var tags = options.tags !== 'none' ? '&tags=' + options.tags : '';
     var url = 'http://www.fortcarsonmountaineer.com/wp-json/wp/v2/posts?per_page=' + options.quantity + categories + tags;
     var output = [];
-    var $template
-    var feedOutput = [];
-    var count = 0;
-    var $newTemplate
-  
-    function outputHtml() {
-      $(domSelection).html(output);
-    }
-  
-    function bindTemplate($newTemplate, data) {
-      $.each(data, function(index, val) {
-        $newTemplate.find('[data-feed-link]').attr('href', val.url);
-        $newTemplate.find('[data-feed-title]').text(val.title);
-        $newTemplate.find('[data-feed-content]').html(val.content);
-        $newTemplate.find('[data-feed-img]').attr('src', val.imgSrc);
-        $newTemplate.find('[data-feed-excerpt]').html(val.excerpt);
-        output.push($newTemplate.prop('outerHTML'));
-      });
-      outputHtml();
-    }
+    var feedOutput = []; 
 
     return this.each(function() {
+      var $this = $(this);
+      var $template = $this.find('[data-feed-template]');
+      var $newTemplate = $template.clone().removeClass(options.hideClass).removeAttr('data-feed-template');
 
-      if (count === 0) {
+      var outputHtml =  function () {
+        $this.html(output);
+      }
+      var bindTemplate = function ($newTemplate, data) {
+        $.each(data, function(index, val) {
+          $newTemplate.find('[data-feed-link]').attr('href', val.url);
+          $newTemplate.find('[data-feed-title]').text(val.title);
+          $newTemplate.find('[data-feed-content]').html(val.content);
+          $newTemplate.find('[data-feed-img]').attr('src', val.imgSrc);
+          $newTemplate.find('[data-feed-excerpt]').html(val.excerpt);
+          output.push($newTemplate.prop('outerHTML'));
+        });
+      }
+
         $.getJSON(url, function (data, textStatus, jqxhr) {
-          count++;
           if (textStatus === 'success') {
-            $template = $(this).find('[data-feed-template]');
-            $newTemplate = $template.clone().removeClass(options.hideClass).removeAttr('data-feed-template');
-            $.each(data, function(key, val) {
-              var feedItem = {
-                'title' : val.title.rendered,
-                'imgSrc' : $(val.content.rendered).find('img').eq(0).attr('src'),
-                'url' : val.link,
-                'excerpt' : val.excerpt.rendered,
-                'content' : val.content.rendered,
-                'tags' : val.tags,
-                'categories' : val.categories
-              }
-              feedOutput.push(feedItem);
-            });
-            bindTemplate($newTemplate, feedOutput);
+            if (feedOutput.length < 1) {
+              $.each(data, function(key, val) {
+                var feedItem = {
+                  'title' : val.title.rendered,
+                  'imgSrc' : $(val.content.rendered).find('img').eq(0).attr('src'),
+                  'url' : val.link,
+                  'excerpt' : val.excerpt.rendered,
+                  'content' : val.content.rendered,
+                  'tags' : val.tags,
+                  'categories' : val.categories
+                }
+                feedOutput.push(feedItem);
+              });
+              bindTemplate($newTemplate, feedOutput);
+            }             
+            outputHtml();
           }
           else {
             $this.html('<p>Uh oh, there is an issue getting the information right now.</p>');
           }  
         });
-      } else {
-        bindTemplate($newTemplate, feedOutput)
-      }
-    });
-
-  }
+    }); //end return
+  }//end $.fn.feed
 
   $.fn.feedMe.defaults = {
     category : 'none',
